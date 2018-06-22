809385b3082873fe1fc30371681cd924a6749c69
Nicolas Blier <nblier@5CG7096H7F>
2018-06-22 16:28:38 -0400

    Added new section on the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -134,7 +134,7 @@ class IndexPage extends Component {
                                                     </Col>
 
                                                     <Col md="3" sm="12">
-                                                        <span className="float-right"><small>{edge.node.date}</small></span>
+                                                        <span className="float-right"><small>{edge.node.dateSpe} / {edge.node.date}</small></span>
                                                     </Col>
                                                 </Row>
                                             </div>
@@ -146,6 +146,37 @@ class IndexPage extends Component {
 
                                                     <Col md="3" sm="12" className="d-flex justify-content-end align-items-end">
                                                         <Link className="float-right mb-2" to={'nouvelles/' + edge.node.slug}>En savoir plus</Link>
+                                                    </Col>
+                                                </Row>
+                                            </div>
+                                        </div>
+                                )
+                            }
+                            <br />
+                            <h2 className="mb-4">Prochaines sorties de chapitre</h2>
+                            {
+                                data.allContentfulCalendrier.edges.map(
+                                    (edge) =>
+                                        <div className="clearfix border-bottom mb-2" key={edge.node.id}>
+                                            <div className="">
+                                                <Row className="no-gutters">
+                                                    <Col md="9" sm="12">
+                                                        <Link to={'nouvelles/' + edge.node.slug}><h3 className="float-left"><small>{edge.node.titre}</small></h3></Link>
+                                                    </Col>
+
+                                                    <Col md="3" sm="12">
+                                                        <span className="float-right"><small>{edge.node.dateSpe} / {edge.node.date}</small></span>
+                                                    </Col>
+                                                </Row>
+                                            </div>
+                                            <div>
+                                                <Row className="no-gutters">
+                                                    <Col md="9" sm="12">
+                                                        <div dangerouslySetInnerHTML={{ __html: edge.node.description.childMarkdownRemark.html }} />
+                                                    </Col>
+
+                                                    <Col md="3" sm="12" className="d-flex justify-content-end align-items-end">
+                                                        <Link className="float-right mb-2" to={edge.node.romanSlug}>Aller voir l'histoire</Link>
                                                     </Col>
                                                 </Row>
                                             </div>
@@ -187,8 +218,25 @@ export const pageQuery = graphql`query listeNouvelleQueryFR {
             }
           }
           date(formatString: "YYYY MMMM DD")
+          dateSpe
           slug
         }
       }
     }
+    allContentfulCalendrier (sort: {fields: [date], order: ASC}, filter: {node_locale: {eq: "fr-CA"}}) {
+		edges {
+			node {
+				id
+				titre
+				description {
+					childMarkdownRemark {
+						html
+					}
+				}
+				date(formatString: "YYYY MMMM DD")
+                dateSpe
+                romanSlug
+			}
+		}
+	}
   }`
