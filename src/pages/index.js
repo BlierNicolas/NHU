e008cdf8f5b1b4e14ec1f067bcb8fe96217776db
Nicolas Blier <nblier@5CG7096H7F>
2018-06-26 12:12:32 -0400

    update calendrier

--- src/pages/index.js
+++ src/pages/index.js
@@ -158,28 +158,33 @@ class IndexPage extends Component {
                                 data.allContentfulCalendrier.edges.map(
                                     (edge) =>
                                         <div className="clearfix border-bottom mb-2" key={edge.node.id}>
-                                            <div className="">
-                                                <Row className="no-gutters">
-                                                    <Col md="9" sm="12">
-                                                        <Link to={'nouvelles/' + edge.node.slug}><h3 className="float-left"><small>{edge.node.titre}</small></h3></Link>
-                                                    </Col>
+                                            {edge.node.affiche ?
+                                                (<div>
+                                                    <div className="">
+                                                        <Row className="no-gutters">
+                                                            <Col md="9" sm="12">
+                                                                <h3 className="float-left"><small>{edge.node.titre}</small></h3>
+                                                            </Col>
 
-                                                    <Col md="3" sm="12">
-                                                        <span className="float-right"><small>{edge.node.dateSpe} / {edge.node.date}</small></span>
-                                                    </Col>
-                                                </Row>
-                                            </div>
-                                            <div>
-                                                <Row className="no-gutters">
-                                                    <Col md="9" sm="12">
-                                                        <div dangerouslySetInnerHTML={{ __html: edge.node.description.childMarkdownRemark.html }} />
-                                                    </Col>
+                                                            <Col md="3" sm="12">
+                                                                <span className="float-right"><small>{edge.node.dateSpe} / {edge.node.date}</small></span>
+                                                            </Col>
+                                                        </Row>
+                                                    </div>
+                                                    <div>
+                                                        <Row className="no-gutters">
+                                                            <Col md="9" sm="12">
+                                                                <div dangerouslySetInnerHTML={{ __html: edge.node.description.childMarkdownRemark.html }} />
+                                                            </Col>
 
-                                                    <Col md="3" sm="12" className="d-flex justify-content-end align-items-end">
-                                                        <Link className="float-right mb-2" to={edge.node.romanSlug}>Aller voir l'histoire</Link>
-                                                    </Col>
-                                                </Row>
-                                            </div>
+                                                            <Col md="3" sm="12" className="d-flex justify-content-end align-items-end">
+                                                                <Link className="float-right mb-2" to={edge.node.romanSlug}>Aller voir l'histoire</Link>
+                                                            </Col>
+                                                        </Row>
+                                                    </div>
+                                                </div>) :
+                                                ('')
+                                            }
                                         </div>
                                 )
                             }
@@ -236,6 +241,7 @@ export const pageQuery = graphql`query listeNouvelleQueryFR {
 				date(formatString: "YYYY MMMM DD")
                 dateSpe
                 romanSlug
+                affiche
 			}
 		}
 	}
