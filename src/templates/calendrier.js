be57335b017c7d121d1f8a6ef2353491cade4346
Nicolas Blier <nblier@5CG7096H7F>
2018-07-17 10:37:12 -0400

    remove unused components

--- src/templates/calendrier.js
+++ src/templates/calendrier.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import MetaTags from 'react-meta-tags';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link'
 import 'bootstrap/dist/css/bootstrap.min.css';
