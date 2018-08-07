b2116b15aef6a0e5ffda9f9043ae5e2aef579e8e
Nicolas Blier <nblier@5CG7096H7F>
2018-08-07 13:32:52 -0400

    update index layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,36 +7,44 @@ import Header from '../components/header'
 import Footer from '../components/footer'
 import '../../css/style.css'
 
-const Layout = ({ children, data }) => (
-    <div>
-        <div>
-            <Helmet
-                htmlAttributes={{ lang: "fr" }}
-                title="L'Univers des Nouveaux Humains"
-                meta={[
-                    { name: 'description', content: 'L\'Univers des Nouveaux Humains' },
-                    { name: 'msapplication-TileColor', content: '#da532c' },
-                    { name: 'theme-color', content: '#007bff' },
-                ]}
-                link={[
-                    { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-                    { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
-                    { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
-                    { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
-                    { rel: "manifest", href: "/site.webmanifest" },
-                    { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
-                ]}
-            />
-            <div id="wrapper">
+class Layout extends Component {
+    render() {
+        const {
+            children
+        } = this.props
+
+        return (
+            <div>
                 <div>
-                    <div>
-                        {children()}
+                    <Helmet
+                        htmlAttributes={{ lang: "fr" }}
+                        title="L'Univers des Nouveaux Humains"
+                        meta={[
+                            { name: 'description', content: 'L\'Univers des Nouveaux Humains' },
+                            { name: 'msapplication-TileColor', content: '#da532c' },
+                            { name: 'theme-color', content: '#007bff' },
+                        ]}
+                        link={[
+                            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+                            { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+                            { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+                            { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+                            { rel: "manifest", href: "/site.webmanifest" },
+                            { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
+                        ]}
+                    />
+                    <div id="wrapper">
+                        <div>
+                            <div>
+                                {children()}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-)
+        )
+    }
+}
 
 Layout.propTypes = {
     children: PropTypes.func,
