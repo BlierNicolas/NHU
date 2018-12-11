c67d69a27eba825e170b36487cb6da1461659e6d
Nicolas Blier <nblier@5CG7096H7F>
2018-12-11 08:40:24 -0500

    push add of survey concept

---
+++ src/components/survey.js
@@ -0,0 +1,136 @@
+import React from 'react'
+import {
+    Row,
+    Input,
+    Label,
+    FormGroup,
+    Button
+} from 'reactstrap';
+import firebase, { auth, provider } from '../firebase.js';
+import 'firebase/database';
+import 'firebase/auth';
+import cookie from 'react-cookies';
+import lang_fr from '../langues/lang_fr.json';
+import lang_en from '../langues/lang_en.json';
+
+export default class Survey extends React.Component {
+    constructor(props) {
+        super(props);
+
+        //this.login = this.login.bind(this);
+
+        /** Buffer de la langue par défaut */
+        this.lang = lang_fr;
+
+        /** Trouve la bonne langue */
+        if (this.props.lang === "fr-CA") { this.lang = lang_fr; }
+        if (this.props.lang === "en-US") { this.lang = lang_en; }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmitSurvey = this.handleSubmitSurvey.bind(this);
+
+        this.state = {
+            user: null,
+            lecteur: "vide",
+            nomRoman: "",
+            surveyAutorise: true,
+            surveyStatus: false,
+            loaded: false
+        };
+
+        this.surveyId = 0;
+
+        if (cookie.load('lecteur_connect') == null) {
+            cookie.save('lecteur_connect', "vide", { path: '/' });
+        }
+
+        if (cookie.load('lecteur_connect') !== "vide") {
+            this.state.lecteur = cookie.load('lecteur_connect')
+        }
+    }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    componentDidMount() {
+        if (typeof window !== "undefined") {
+            const itemsRef = firebase.database().ref('survey');
+            itemsRef.on('value', (snapshot) => {
+                let items = snapshot.val();
+                let newState = [];
+                for (let item in items) {
+                    newState.push({
+                        id: item,
+                        user: this.state.lecteur.email,
+                        reponse: this.state.question1,
+                        surveyId: this.surveyId
+                    });
+                }
+
+                this.items = newState
+
+                if (!this.state.loaded) {
+                    //this.checkUpLikes();
+                    this.setState({ loaded: true });
+                }
+            });
+        }
+    }
+
+    handleSubmitSurvey(e) {
+        if (typeof window !== "undefined") {
+            // Empêche le refresh
+            e.preventDefault();
+
+            // Met la référence vers la database
+            const itemsRef = firebase.database().ref('survey');
+
+            console.log(this.state.question1)
+
+            if ((!this.surveyStatus) && (this.state.loaded)) {
+                this.items.map((item) =>
+                    (item.user === this.state.lecteur.email) ?
+                        this.setState({ surveyAutorise: false }) : ''
+                )
+
+                if (this.state.surveyAutorise) {
+                    // Popule les champs dans une collection "item"
+                    const item = {
+                        user: this.state.lecteur.email,
+                        reponse: this.state.question1,
+                        surveyId: this.surveyId
+                    }
+
+                    console.log("submit: " + this.state.question1)
+
+                    // Pousse l'item créé dans la collection
+                    itemsRef.push(item);
+                }
+            }
+
+            //this.toggleLike();
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Row>
+                    <h2>Survey</h2>
+                </Row>
+                <Row>
+                    <form onSubmit={this.handleSubmitSurvey}>
+                        <FormGroup>
+                            <Label for="question1">Question 1</Label>
+                            <Input type="text" name="question1" id="question1" onChange={this.handleChange} placeholder="Réponse question 1" />
+                        </FormGroup>
+                        <Button>Submit</Button>
+                    </form>
+                </Row>
+            </div>
+        );
+    }
+}
